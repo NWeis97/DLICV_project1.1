707e447598a4c27fc06315768721d89e89f16135
NWeis97 <nicolai.weisbjerg@gmail.com>
2022-06-03 16:36:06 +0200

    EffecientNet model init

--- src/models/train_model_effecientNet.py
+++ src/models/train_model_effecientNet.py
@@ -13,6 +13,8 @@ from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
+import pdb
+
 
 # Get CUDA
 if torch.cuda.is_available():
@@ -51,7 +53,7 @@ class Hotdog_NotHotdog(torch.utils.data.Dataset):
 
 
 # Load data
-size = 128
+size = 224
 train_transform = transforms.Compose([transforms.Resize((size, size)), 
                                     transforms.ToTensor()])
 test_transform = transforms.Compose([transforms.Resize((size, size)), 
@@ -62,3 +64,56 @@ trainset = Hotdog_NotHotdog(train=True, transform=train_transform)
 train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=3)
 testset = Hotdog_NotHotdog(train=False, transform=test_transform)
 test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=3)
+
+
+# Get model
+efficientnet = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_efficientnet_b0', pretrained=True)
+# Freeze parameters for pretrained model (otherwise overfitting like crazy)
+for param in efficientnet.parameters():
+    param.requires_grad = False
+efficientnet.to(device)
+
+# Change model classifier to hotdog/notdog (don't freeze params of last layer)
+num_ftrs = efficientnet.classifier.fc.in_features
+efficientnet.classifier.fc = nn.Linear(num_ftrs, 2)
+
+# Set optimizer and model
+model = efficientnet.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.003)
+
+
+num_epochs = 10
+
+for epoch in tqdm(range(num_epochs), unit='epoch'):
+    #For each epoch
+    train_correct = 0
+    model.train()
+    for minibatch_no, (data, target) in tqdm(enumerate(train_loader), total=len(train_loader)):
+        data, target = data.to(device), target.to(device)
+        #Zero the gradients computed for each weight
+        optimizer.zero_grad()
+        #Forward pass your image through the network
+        output = model(data)
+        #Compute the loss
+        loss = F.nll_loss(F.log_softmax(output, dim=1), target)
+        #Backward pass through the network
+        loss.backward()
+        #Update the weights
+        optimizer.step()
+        
+        #Compute how many were correctly classified
+        predicted = output.argmax(1)
+        train_correct += (target==predicted).sum().cpu().item()
+    #Comput the test accuracy
+    model.eval()
+    test_correct = 0
+    for data, target in test_loader:
+        data = data.to(device)
+        with torch.no_grad():
+            output = model(data)
+        predicted = output.argmax(1).cpu()
+        test_correct += (target==predicted).sum().item()
+    train_acc = train_correct/len(trainset)
+    test_acc = test_correct/len(testset)
+    print("Accuracy train: {train:.1f}%\t test: {test:.1f}%".format(test=100*test_acc, train=100*train_acc))
+
