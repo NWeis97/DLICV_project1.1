75cebb9f0cb4e817843ed9069f35d1c11ac3f2c1
NWeis97 <nicolai.weisbjerg@gmail.com>
2022-06-03 14:16:03 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a git repo for project 1.1 for group 6 in 02514 Deep Learning in Computer vision',
+    author='Group_6',
+    license='MIT',
+)
