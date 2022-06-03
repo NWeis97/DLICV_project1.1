c00962de4319b0392242c6498b0b726eb7068a89
NWeis97 <nicolai.weisbjerg@gmail.com>
2022-06-03 14:56:33 +0200

    With Venv

---
+++ src/models/train_model.py
@@ -0,0 +1,21 @@
+# Import 
+import os
+import numpy as np
+import glob
+import PIL.Image as Image
+from tqdm.notebook import tqdm
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+# Get CUDA
+if torch.cuda.is_available():
+    print("The code will run on GPU.")
+else:
+    print("The code will run on CPU. Go to Edit->Notebook Settings and choose GPU as the hardware accelerator")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
